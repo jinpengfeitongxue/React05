5730a1a5392bb1df591e6c3b4e0ece41f7f436fb
majinpeng <2061363265@qq.com>
2020-11-10 20:05:34 +0800

    react05
    Signed-off-by: majinpeng <2061363265@qq.com>

---
+++ build/precache-manifest.e42a0189fac8fa8ee9157afca40bc62c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4dc180674e7858e0f246fb2238ce176",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dc7009e15b42e06cc311",
+    "url": "./static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "fd94a110bad7918e1585",
+    "url": "./static/css/main.f0a27787.chunk.css"
+  },
+  {
+    "revision": "dc7009e15b42e06cc311",
+    "url": "./static/js/2.33cf914b.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "./static/js/2.33cf914b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd94a110bad7918e1585",
+    "url": "./static/js/main.26d9eb2e.chunk.js"
+  },
+  {
+    "revision": "705f434324a29d64b932",
+    "url": "./static/js/runtime-main.075efe5b.js"
+  }
+]);
